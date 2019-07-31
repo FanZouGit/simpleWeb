fa1f09f625e8f9156a6e0eab43b5b06468b6eb74
Fan Zou <fanzou@Fans-MacBook-Pro.local>
2019-07-31 15:08:40 -0400

    modify App.js in br1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          already learned React
         </a>
       </header>
     </div>
